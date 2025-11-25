c5a79946638dca31c0bb9027fd5557212fbc17fe
Khushi-codingal <rashmiabc@gmail.com>
2025-11-25 15:09:12 +0530

    Raju

---
+++ project2.py
@@ -0,0 +1,15 @@
+#let's check the birth  dates of raju's friends
+swetha = 21/12/2013
+print("type of swetha : ", type(swetha))
+
+murthy = 19/10/2013
+print("type of murthy : ", type(murthy))
+
+kanaka = 25/11/2014
+print("type of kanaka : ", type(kanaka))
+
+daren = 15/3/2016
+print("type of daren : ", type(daren))
+
+spike = 13/10/2021
+print("type of spike : ", type(spike))
