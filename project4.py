b384eb9dd823aac3fc9b4d7099d88e01fc387fda
Khushi-codingal <rashmiabc@gmail.com>
2025-11-28 10:12:30 +0530

    square root

---
+++ project4.py
@@ -0,0 +1,13 @@
+import math
+
+number = 25
+square_root = math.sqrt(number)
+print(f"The square root of {number} is {square_root}") 
+number = 36
+square_root = number ** 0.5
+print(f"The square root of {number} is {square_root}")
+import cmath
+
+number = -9
+square_root = cmath.sqrt(number)
+print(f"The square root of {number} is {square_root}")
