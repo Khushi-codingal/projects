f7214c225e445b5b7446a03c3ed5c68934805cee
Khushi-codingal <rashmiabc@gmail.com>
2025-12-06 13:39:47 +0530

    base

---
+++ project9.py
@@ -0,0 +1,5 @@
+base = float(input("Enter the base number: "))
+exponent = int(input("Enter the exponent (n): "))
+
+result = pow(base, exponent)
+print(f"The result of {base} raised to the power of {exponent} is: {result}")
