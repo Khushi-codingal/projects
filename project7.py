4951ffbd6071a62c38c47bc0b1a5169e4bf2db71
Khushi-codingal <rashmiabc@gmail.com>
2025-12-06 13:26:33 +0530

    num_digits

---
+++ project7.py
@@ -0,0 +1,11 @@
+user_input = input("Enter a number: ")
+num_digits = len(user_input)
+print("The number of digits in your input is:", num_digits)
+# Get input from the user
+user_input = input("Enter a number: ")
+
+# Calculate the number of digits by finding the length of the string
+num_digits = len(user_input)
+
+# Print the result
+print("The number of digits in your input is:", num_digits)
