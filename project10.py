48c23c8d135593b9c8b1ea9427cdd6802e7fba6b
Khushi-codingal <rashmiabc@gmail.com>
2025-12-07 14:55:13 +0530

    age

---
+++ project10.py
@@ -0,0 +1,5 @@
+age = int(input("Please enter your age: "))
+if 10 <= age <= 20:
+        print("The age entered is between 10 and 20 years.")
+else:
+        print("The age entered is NOT between 10 and 20 years.")
